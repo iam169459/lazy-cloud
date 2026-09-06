6a54412d7f5b546a331d82f71eea5204b6429059
iam169459 <iam169459@gmail.com>
2026-09-06 06:24:05 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,8 @@ export default defineConfig(({ mode }) => {
       exclude: ['lucide-react'],
     },
     server: {
+      host: '0.0.0.0',
+      port: 5173,
       fs: {
         allow: ['..'],
       },
